2a0d4586b083f642e7d5db38f43cf361f73e63fd
King Portipher <kingportipher@gmail.com>
2024-06-10 00:50:08 +0000

    Task done

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,29 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Retrieve one object based on class and ID"""
+        if cls and id:
+
+            if cls in classes.value() and isinstance(id, str):
+                all_objects = self.all(cls)
+                for key, value in all_objects.items():
+                    if key.split('.')[1] == id:
+                        return value
+            else:
+                return
+        return
+
+
+
+    def count(self, cls=None):
+        """Count the number of objects in storage"""
+        if not cls:
+            inst_of_all_cls = self.all()
+            return len(inst_of_all_cls)
+        if cls in classes.values():
+            all_inst_of_prov_cls = self.all(cls)
+            return len(all_inst_of_prov_cls)
+        if cls not in classes.values():
+            return 
