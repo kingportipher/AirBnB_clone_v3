91a8897428daac242badf1f6690dfc51b98858ef
King Portipher <kingportipher@gmail.com>
2024-06-11 02:47:26 +0000

    Task 4 done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ Create Flask app
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status', methods=['GET', ])
 def api_status():
@@ -13,3 +14,18 @@ def api_status():
     """
     response = {'status': 'OK'}
     return jsonify(response)
+
+@app_views.route('/stats')
+def get_stats():
+    """
+    """
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+
+    return jsonify(stats)
